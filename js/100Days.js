e53ae18c6dacebd9ef224ae821fc6b9c2f47f319
Eduardo Francisco Garcia <lalito9270@gmail.com>
2023-07-10 13:24:14 -0600

    empiezo

--- js/100Days.js
+++ js/100Days.js
@@ -10,6 +10,16 @@ let $menu2 = d.querySelector(".dropDown svg");
 let $panel = d.querySelector(".panel");
 let $hideMenu = d.querySelector(".hideMenu");
 
+let $open = d.querySelector(".phot");
+let $bottom = d.querySelector(".infoBottom");
+let $top = d.querySelector(".infoTop");
+let $imgTop = d.querySelector(".imageTransition");
+let $containerTopBottom = d.querySelector(".infoPhotoTransition");
+let $close = d.querySelector(".closeInfoTransition");
+
+let $obsession = d.querySelector(".obsession");
+let $infoBlock = d.querySelector(".infoBlock");
+
 d.addEventListener("click", (e) => {
   if (e.target === $menu) {
     $bar1.classList.toggle("activeMenu1");
@@ -26,7 +36,30 @@ d.addEventListener("click", (e) => {
     $panel.classList.toggle("move");
     $hideMenu.classList.toggle("move2");
   }
+
+  if (e.target === $open) {
+    $top.classList.add("moveTop");
+    $bottom.classList.add("moveBottom");
+    $imgTop.classList.add("moveImg");
+    $containerTopBottom.classList.add("displayContainer");
+  }
+
+  if (e.target === $close) {
+    $top.classList.remove("moveTop");
+    $bottom.classList.remove("moveBottom");
+    $imgTop.classList.remove("moveImg");
+    $containerTopBottom.classList.remove("displayContainer");
+  }
 });
 
+d.addEventListener("mouseover", (e) => {
+  if (e.target === $obsession) {
+    $infoBlock.classList.add("activeInfoBlock");
+  }
+});
 
-
+d.addEventListener("mouseout", (e) => {
+  if (e.target === $obsession) {
+    $infoBlock.classList.remove("activeInfoBlock");
+  }
+});
